20995b769cc3752bbe37e3f8ed310ccf1d6acf21
Fan Duc'k <81787710+Gerphan94@users.noreply.github.com>
2024-04-16 21:21:11 +0700

    1
    1

--- client/src/Component/MarkDownReader.js
+++ client/src/Component/MarkDownReader.js
@@ -17,8 +17,6 @@ const MarkdownReader = ({ filename }) => {
         }
         )
        
-
-
     return (
         <div>
             <Markdown >
--- client/src/Component/SQLCollection.js
+++ client/src/Component/SQLCollection.js
@@ -8,11 +8,19 @@ function SQLCol() {
     return (
         <>
             <div>SQL</div>
-            <div className='px-4'>
-                <div className='border text-left'>
-                    <MarkdownReader filename="dutru.md" />
+            <div className='flex'>
+                <div className='px-4 w-auto '>
+                    <div className='border text-left'>
+                        <MarkdownReader filename="dutru.md" />
+                    </div>
+
                 </div>
+                <div className='bg-gray-400 h-full w-48 ml-auto'>
+aaa
             </div>
+            </div>
+            
+
 
 
 
