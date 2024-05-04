ab192cd9043c48e2de5f642f72030ac4188f885f
Fan Duc'k <gerphan94@gmail.com>
2024-05-04 11:37:08 +0700

    1
    1

--- client/src/Component/KPI/KPI.js
+++ client/src/Component/KPI/KPI.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from "react";
 import DKDat from "./Dieukiendat";
 import { MdDelete } from "react-icons/md";
 import { NumericFormat } from 'react-number-format';
+import { IoMdAddCircle } from "react-icons/io";
 
 function KPI() {
-    
+
+    const opts = ["<", "<=", "=", ">=", ">"]
+
     const [kpis, setKPIS] = useState([
         {
             "noidung": "Đạt số lượng tổng Khách hàng khám trung bình một ngày \ntheo quy định bệnh viện (A)",
@@ -51,14 +54,14 @@ function KPI() {
                 selectedOption: '>=',
                 value: 94
             }
-            
+
 
             ],
             "ketqua": [{
                 name: '',
                 selectedOption: '',
                 value: null
-            
+
             }
             ], "trangthai": 0
         }
@@ -104,7 +107,7 @@ function KPI() {
             "cachthuc": formData.cachthuc,
             "dieukien": [],
             "ketqua": [],
-        "trangthai": 0
+            "trangthai": 0
         }
         setKPIS(prevData => [...prevData, newData]);
 
@@ -115,51 +118,51 @@ function KPI() {
         })
         // Perform further actions with the data as needed
     };
-  
+
 
     return (
         <>
             <div>KPI</div>
             <form onSubmit={(e) => handleAdd(e)}>
-            <div className="grid grid-cols-3 w-full p-4">
-                <div className="text-left w-full p-4">
-                    <label className="font-bold">Nội dung đánh giá</label>
-                    <textarea 
-                    name="noidung"
-                    value={formData.noidung}
-                    onChange={handleChange}
-                    className="border outline-none p-2 block w-full mt-2" rows={3}
-                    required={true}
-                     />
+                <div className="grid grid-cols-3 w-full p-4">
+                    <div className="text-left w-full p-4">
+                        <label className="font-bold">Nội dung đánh giá</label>
+                        <textarea
+                            name="noidung"
+                            value={formData.noidung}
+                            onChange={handleChange}
+                            className="border outline-none p-2 block w-full mt-2" rows={3}
+                            required={true}
+                        />
 
-                </div>
-                <div className="text-left p-4">
-                    <label className="font-bold" >Cách thức đo lường</label>
-                    <textarea
-                    name="cachthuc"
-                    className="border outline-none p-2 block w-full mt-2" rows={3}
-                    value={formData.cachthuc}
-                    onChange={handleChange}
-                    required={true}
-                    
-                    />
-                </div>
-                <div className="text-left p-4">
-                    <label className="font-bold" >Điều kiện đạt</label>
-                    <div className="mt-2">
-                        <DKDat dkData={dkData} setDkData={setDkData} />
                     </div>
-                </div>
-                <div className="flex">
-                    <button 
-                    className="w-24 px-2 py-1 bg-blue-500 text-white hover:bg-blue-600"
-                    type="submit"
-                    >Thêm</button>
-                </div>
+                    <div className="text-left p-4">
+                        <label className="font-bold" >Cách thức đo lường</label>
+                        <textarea
+                            name="cachthuc"
+                            className="border outline-none p-2 block w-full mt-2" rows={3}
+                            value={formData.cachthuc}
+                            onChange={handleChange}
+                            required={true}
 
-            </div>
+                        />
+                    </div>
+                    <div className="text-left p-4">
+                        <label className="font-bold" >Điều kiện đạt</label>
+                        <div className="mt-2">
+                            <DKDat dkData={dkData} setDkData={setDkData} />
+                        </div>
+                    </div>
+                    <div className="flex">
+                        <button
+                            className="w-24 px-2 py-1 bg-blue-500 text-white hover:bg-blue-600"
+                            type="submit"
+                        >Thêm</button>
+                    </div>
+
+                </div>
             </form>
-            
+
             <div>
                 <div className="w-full bg-blue-600 text-white text-left px-2 py-1">Tiêu chí điều kiện cần</div>
                 <div>
@@ -175,7 +178,7 @@ function KPI() {
                             </tr>
                         </thead>
                         <tbody>
-                            {kpis.length > 0 &&  kpis.map((kpi, index) =>
+                            {kpis.length > 0 && kpis.map((kpi, index) =>
                                 <tr className="border-b py-2">
                                     <td>
                                         <div className="flex justify-center items-center flex-col">
@@ -202,39 +205,55 @@ function KPI() {
                                             style={{ height: textareaHeight }}>
                                             {kpi.dieukien.map((ele) =>
                                                 <div className="flex gap-2 mb-1">
-                                                    <div className="w-1/2 text-left">{ele.name}</div>
-                                                    <div className="w-1/4">{ele.selectedOption}</div>
-                                                    <div className="w-1/4 border">{ele.value}</div>
+                                                    <div className="flex justify-center items-center text-red-600"><MdDelete /></div>
+                                                    <div className="w-1/2 text-left border px-2 py-1">{ele.name}</div>
+                                                   
+                                                        <select className="border outline-none">
+                                                            {opts.map((opt, index) => (
+                                                                <option key={index} value={opt}>{opt}</option>
+                                                            ))}
+                                                        </select>
+
+
+                                                    
+                                                    <input 
+                                                    className="w-1/4 border px-2 py-1 outline-none" 
+                                                    type="number"
+                                                    />
                                                 </div>
                                             )}
+                                            <div className="flex">
+                                                <button className=" text-blue-500">
+                                                    <IoMdAddCircle />
+                                                </button>
+                                            </div>
+
                                         </div>
                                     </td>
                                     <td>
                                         <div className="p-4 border min-h-28">
                                             {kpi.ketqua.map((ele) =>
                                                 <div className="flex gap-2 mb-1">
-                                                    <div className="w-1/2 text-left">{ele.name}</div>
-                                                    <div className="w-1/4">{ele.selectedOption}</div>
-                                                    
+
+
                                                     <input
                                                         type="number"
-                                                        className="w-16 border outline-none px-2 bg-orange-100"
-                                                        style={{ '-moz-appearance': 'textfield', '::-webkit-inner-spin-button': 'display: none', '::-webkit-outer-spin-button': 'display: none' }}
-                                                        value={ele.value}
+                                                        className="w-full border outline-none  bg-orange-100 px-2 py-1"
+
                                                     />
                                                 </div>
                                             )}
                                         </div>
                                     </td>
                                     <td>
-                                        <div className="border ">
-                                        <select className="border px-2 py-1 bg-blue-200 rounded-md">
-                                            <option className="bg-white py-2"></option>
-                                            <option className="bg-white py-2">Đạt</option>
-                                            <option className="bg-white py-2">Không Đạt</option>
-                                        </select>
+                                        <div className="border min-h-28 flex justify-center items-center ">
+                                            <select className="border px-2 py-1 bg-blue-200 rounded-md">
+                                                <option className="bg-white py-2"></option>
+                                                <option className="bg-white py-2">Đạt</option>
+                                                <option className="bg-white py-2">Không Đạt</option>
+                                            </select>
                                         </div>
-                                        
+
                                     </td>
                                 </tr>
                             )}
