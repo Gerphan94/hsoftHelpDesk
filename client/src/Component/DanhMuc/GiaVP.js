0cb3c8756630b4f16a88f5d1a83710089f5645be
Fan Duc'k <gerphan94@gmail.com>
2024-07-05 16:48:25 +0700

    Update GiaVP.js

--- client/src/Component/DanhMuc/GiaVP.js
+++ client/src/Component/DanhMuc/GiaVP.js
@@ -4,6 +4,7 @@ import ViewButton from "../Button/ViewButton";
 import { FaPlus, FaMinus, FaDotCircle } from "react-icons/fa";
 import { FaFolderTree } from "react-icons/fa6";
 
+import { FcCurrencyExchange, FcFlowChart, FcMoneyTransfer  } from "react-icons/fc";
 
 function GiaVP({ site }) {
 
@@ -20,11 +21,12 @@ function GiaVP({ site }) {
     const [selectedLoaiVP, setSelectedLoaiVP] = useState(0);
 
     const giavpTypes = [
-        { id: 'giavp', name: 'Giá viện phí' },
-        { id: 'goivp', name: 'Gói viện phí' },
-        { id: 'dieutiet', name: 'Điều tiết' }
+        { id: 'giavp', name: 'Giá viện phí', icon: FcCurrencyExchange },
+        { id: 'goivp', name: 'Gói viện phí', icon: FcMoneyTransfer },
+        { id: 'dieutiet', name: 'Điều tiết', icon: FcFlowChart }
     ]
-    const [showType, setShowType] = useState('giavp');
+
+    const [isShowGiaVP, setIsShowGiaVP] = useState(true);
 
 
 
@@ -74,6 +76,14 @@ function GiaVP({ site }) {
         }
     }
     const handleClick3 = async (id) => {
+        if (id === 'giavp') {
+            setIsShowGiaVP(!isShowGiaVP);
+
+        }
+        else {
+            setIsShowGiaVP(false);
+        }
+        
         setShowIndex(id);
     }
 
@@ -84,51 +94,53 @@ function GiaVP({ site }) {
             <div>
                 <div className="mt-2 " >
                     <div className="flex lg:h-[750px]">
-                        <div className="overflow-y-scroll">
-                            {treeLoaiVps.map((item) => (
-                                <div className="text-left text-[#17273D] ">
-                                    <div className={`px-2 py-1  hover:bg-[#667BC6] hover:border-b-white select-none ${showIndex === item.id ? 'bg-[#667BC6]' : ''}`}
-                                        onClick={() => handleClick1(item.id)}>
-                                        <div className="flex items-center gap-1">
-                                            {item.child.length > 0 ?
-                                                showIndex === item.id ?
-                                                    <FaMinus className="text-sm" /> :
-                                                    <FaPlus className="text-sm" />
-                                                : <FaPlus className="text-sm text-transparent" />}
-                                            {item.name}
-                                        </div>
-                                    </div>
-                                    {showIndex === item.id && (
-                                        <div className="border-lime-300 ml-5">
-                                            {item.child.map((item1) => (
-                                                <div
-                                                    className={`flex justify-between px-2 py-1 select-none hover:bg-[#B6BBC4] ${selectedLoaiVP === item1.id ? 'bg-[#B6BBC4]' : ''}`}
-                                                    onClick={() => handleClick2(item1.id)}
-                                                >
-                                                    <div>
-                                                        {item1.name}
-                                                    </div>
-                                                    <span className="rounded-xl px-2 text-sm">{item1.total}</span>
-                                                </div>
-                                            ))}
-                                        </div>
-                                    )}
-
-
-                                </div>
-
-                            ))}
-
+                        <div className="w-96 overflow-y-scroll">
                             {giavpTypes.map((item) => (
-                                <div
-                                    className={`text-left px-2 py-1  hover:bg-[#667BC6] hover:border-b-white select-none ${showIndex === item.id ? 'bg-[#667BC6]' : ''}`}
-                                    onClick={() => handleClick3(item.id)}
-                                >
-                                    <div className="flex gap-1 items-center">
-                                        <FaFolderTree />
-                                        <div> {item.name}</div>
+                                <div>
+                                    <div
+                                        className={`text-left px-2 py-1  hover:bg-[#667BC6] hover:border-b-white select-none ${showIndex === item.id ? 'bg-[#667BC6] text-white' : ''}`}
+                                        onClick={() => handleClick3(item.id)}
+                                    >
+                                        <div className="flex gap-1 items-center">
+                                            {<item.icon  />}
+                                            <div> {item.name}</div>
+                                        </div>
                                     </div>
+                                    
+                                    {isShowGiaVP && item.id === 'giavp' &&  treeLoaiVps.map((item) => (
+                                            <div className="ml-2 text-left text-[#17273D] ">
+                                                <div className={`px-2 py-1  hover:bg-[#667BC6] hover:border-b-white select-none ${showIndex === item.id ? 'bg-[#667BC6]' : ''}`}
+                                                    onClick={() => handleClick1(item.id)}>
+                                                    <div className="flex items-center gap-1">
+                                                        {item.child.length > 0 ?
+                                                            showIndex === item.id ?
+                                                                <FaMinus className="text-sm" /> :
+                                                                <FaPlus className="text-sm" />
+                                                            : <FaPlus className="text-sm text-transparent" />}
+                                                        {item.name}
+                                                    </div>
+                                                </div>
+                                                {showIndex === item.id && (
+                                                    <div className="border-lime-300 ml-5">
+                                                        {item.child.map((item1) => (
+                                                            <div
+                                                                className={`flex justify-between px-2 py-1 select-none hover:bg-[#B6BBC4] ${selectedLoaiVP === item1.id ? 'bg-[#B6BBC4]' : ''}`}
+                                                                onClick={() => handleClick2(item1.id)}
+                                                            >
+                                                                <div>
+                                                                    {item1.name}
+                                                                </div>
+                                                                <span className="rounded-xl px-2 text-sm">{item1.total}</span>
+                                                            </div>
+                                                        ))}
+                                                    </div>
+                                                )}
+                                            </div>
+
+                                        ))}
+
                                 </div>
+
                             ))}
 
 
